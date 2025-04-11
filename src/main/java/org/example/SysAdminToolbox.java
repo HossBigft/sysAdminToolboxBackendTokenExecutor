788798c99d5d14cd29c371d39f80829efc7b1974
Maksim Gluwenko <maksim_gluwenko@epam.com>
2025-04-11 08:07:57 +0500

    Extract get email into own function

--- src/main/java/org/example/SysAdminToolbox.java
+++ src/main/java/org/example/SysAdminToolbox.java
@@ -5,17 +5,55 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Parameters;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.Callable;
+import java.util.function.Predicate;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 @Command(name = "sysadmintoolbox",
         description = "Executes sudo commands on server",
         mixinStandardHelpOptions = true
 )
 public class SysAdminToolbox implements Callable<Integer> {
+    private static final String TEST_MAIL_LOGIN="testsupportmail";
+    private static final String MAIL_DESCRIPTION="throwaway mail for troubleshooting purposes. You may delete it at will.";
+    private static final int MAIL_PASSWORD_LENGTH=15;
     private static final Pattern DOMAIN_PATTERN =
             Pattern.compile("^(?!-)[A-Za-z0-9-]{1,63}(?<!-)\\.(?!-)[A-Za-z0-9.-]{2,}$");
+
+    Predicate<String> isDomain= DOMAIN_PATTERN.asMatchPredicate();
+
+    private Optional<String> getEmailPassword(String login, String domain) {
+        String emailPassword = "";
+
+        ProcessBuilder builder = new ProcessBuilder("/usr/local/psa/admin/bin/mail_auth_view");
+        try {
+            Process process = builder.start();
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+                List<String> result = reader.lines()
+                        .filter(line -> line.contains(login + "@" + domain))
+                        .map(line -> line.replaceAll("\\s", "")) // remove all whitespace
+                        .map(line -> {
+                            int index = line.indexOf('|');
+                            return index >= 0 ? line.split("\\|")[3] : "";
+                        })
+                        .toList();
+
+                if (!result.isEmpty()) {
+                    emailPassword = result.get(0);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("/usr/local/psa/admin/bin/mail_auth_view is not found");
+        }
+
+        return emailPassword.isEmpty() ? Optional.empty() : Optional.of(emailPassword);
+    }
 
     @Parameters(index = "0", description = "The email login (before the @).")
     private String testMailLogin;
@@ -30,18 +68,13 @@ public class SysAdminToolbox implements Callable<Integer> {
             return 1;
         }
 
-        ProcessBuilder builder = new ProcessBuilder("/usr/local/psa/admin/bin/mail_auth_view");
-        Process process = builder.start();
+        Optional<String> maybePassword = getEmailPassword(testMailLogin, domain);
 
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
-            reader.lines()
-                    .filter(line -> line.contains(testMailLogin + "@" + domain))
-                    .map(line -> line.replaceAll("\\s", "")) // remove all whitespace
-                    .map(line -> {
-                        int index = line.indexOf('|');
-                        return index >= 0 ? line.split("\\|")[3] : "";
-                    })
-                    .forEach(System.out::println);
+        if (maybePassword.isPresent()) {
+            System.out.println(maybePassword.get());
+        } else {
+            System.err.println("No password found for " + testMailLogin + "@" + domain);
+            return 0;
         }
 
         return 0;
