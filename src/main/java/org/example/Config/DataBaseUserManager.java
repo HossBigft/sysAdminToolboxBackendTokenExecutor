bf11efcb9205fff474fb294403ed623cf0c7d572
Maksim Gluwenko <maksim_gluwenko@epam.com>
2025-04-22 21:27:08 +0500

    Refactor db user management

--- src/main/java/org/example/Config/DataBaseUserManager.java
+++ src/main/java/org/example/Config/DataBaseUserManager.java
@@ -9,7 +9,6 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Arrays;
 import java.util.List;
 
 public class DataBaseUserManager {
@@ -27,7 +26,7 @@ public class DataBaseUserManager {
                     ConfigManager.updateDotEnv();
                 }
             }
-        } catch (CommandFailedException e){
+        } catch (CommandFailedException e) {
             LogManager.log().action("Failed to ensure database user", databaseUser).error(e);
         }
     }
@@ -50,37 +49,40 @@ public class DataBaseUserManager {
             List<String> output = ShellUtils.runCommand(command);
             boolean exists = output.getFirst().trim().equals("1");
 
-            LogManager.log().action("Check if database user is present", databaseUser, exists);
+            if (exists) {
+                LogManager.log().debug("Database user " + databaseUser + " is present.");
+            } else {
+                LogManager.log().info("Database user " + databaseUser + " does not exist.");
+            }
+
             return exists;
         } catch (CommandFailedException e) {
-            LogManager.log().command( command).error(e);
+            LogManager.log().command(command).error(e);
             throw e;
         }
     }
 
-    private void createUser() {
-        String mysqlCliName;
+    void createUser() throws CommandFailedException {
+        String mysqlCliName = ShellUtils.getSqlCliName();
+        String query = String.format(
+                "CREATE USER '%s'@'localhost' IDENTIFIED BY '%s'; FLUSH PRIVILEGES;",
+                databaseUser, databasePassword);
+
+        String[] command = new String[]{
+                mysqlCliName,
+                "-u", DatabaseProvisioner.ADMIN_USER,
+                "-e", query
+        };
+
         try {
-            mysqlCliName = ShellUtils.getSqlCliName();
-            String[] command = new String[]{mysqlCliName, "-u", DatabaseProvisioner.ADMIN_USER, "-e",
-                    String.format("CREATE USER '%s'@'localhost' IDENTIFIED BY '%s'; FLUSH PRIVILEGES;",
-                            databaseUser, databasePassword)};
-            try {
-
-                ShellUtils.runCommand(command);
-
-                LogManager.log().action("Create database user", databaseUser, true).info();
-            } catch (CommandFailedException e) {
-                LogManager.log()
-                        .command(command)
-                        .error(e);
-            }
+            ShellUtils.runCommand(command);
+            LogManager.log().info("Created database user " + databaseUser);
         } catch (CommandFailedException e) {
-            LogManager.log()
-                    .action("Create database user", databaseUser, false)
-                    .error(e);
+            LogManager.log().command(command).error(e);
+            throw e;
         }
     }
+
 
     private boolean isDbUserAbleToConnect() {
         try (Connection conn = DriverManager.getConnection(DatabaseProvisioner.DB_URL, databaseUser,
@@ -97,33 +99,33 @@ public class DataBaseUserManager {
         databasePassword = Utils.generatePassword(ConfigManager.DB_USER_PASSWORD_LENGTH);
         ConfigManager.values.put("DATABASE_PASSWORD", databasePassword
         );
-        LogManager.log().action("Regenerate password for database user", databaseUser).info();
+        LogManager.log().info("Regenerate password for database user " + databaseUser);
     }
 
-    private void setDbUserPassword() {
+    void setDbUserPassword() throws CommandFailedException {
         if (databaseUser.equalsIgnoreCase(DatabaseProvisioner.ADMIN_USER)) {
             LogManager.log()
-                    .warn("WARNING: Refusing to modify the root database user. Please configure a different database user");
+                    .warn("WARNING: Refusing to modify the root database user password. Please configure a different database user");
             return;
         }
+
+        String mysqlCliName = ShellUtils.getSqlCliName();
+        String query = String.format(
+                "ALTER USER '%s'@'localhost' IDENTIFIED BY '%s'; FLUSH PRIVILEGES;",
+                databaseUser, databasePassword);
+
+        String[] command = new String[]{
+                mysqlCliName,
+                "-u", DatabaseProvisioner.ADMIN_USER,
+                "-e", query
+        };
+
         try {
-            String[] command = new String[]{ShellUtils.getSqlCliName(), "-u", DatabaseProvisioner.ADMIN_USER, "-e",
-                    String.format("ALTER USER '%s'@'localhost' IDENTIFIED BY '%s'; FLUSH PRIVILEGES;",
-                            databaseUser, databasePassword)};
-            try {
-                ShellUtils.runCommand(command);
-
-                LogManager.log().action("Set database user password", databaseUser, true).info();
-            } catch (CommandFailedException e) {
-
-                LogManager.log()
-                        .command(command)
-                        .error(e);
-            }
+            ShellUtils.runCommand(command);
+            LogManager.log().info("Set database user password " + databaseUser);
         } catch (CommandFailedException e) {
-            LogManager.log()
-                    .action("Create database user", databaseUser, false)
-                    .error(e);
+            LogManager.log().command(command).error(e);
+            throw e;
         }
     }
 }
