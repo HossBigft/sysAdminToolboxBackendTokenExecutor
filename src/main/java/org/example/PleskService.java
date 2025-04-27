3aec036140f0cfa56754f57759ca4b305012e601
Maksim Gluwenko <maksim_gluwenko@epam.com>
2025-04-27 21:57:49 +0500

    Remove nonexisting command

--- src/main/java/org/example/PleskService.java
+++ src/main/java/org/example/PleskService.java
@@ -6,15 +6,11 @@ import org.example.commands.plesk.PleskFetchSubscriptionInfoCommand;
 import org.example.commands.plesk.PleskGetLoginLinkCommand;
 import org.example.commands.plesk.PleskGetTestMailboxCommand;
 import org.example.exceptions.CommandFailedException;
-import org.example.utils.ShellUtils;
 import org.example.value_types.DomainName;
 import org.example.value_types.LinuxUsername;
 
 import java.sql.SQLException;
-import java.util.List;
 import java.util.Optional;
-
-import static org.example.constants.PleskConstants.PLESK_CLI_EXECUTABLE;
 
 public class PleskService {
 
@@ -28,10 +24,7 @@ public class PleskService {
             CommandFailedException, SQLException {
         return new PleskGetLoginLinkCommand(subscriptionId, username).execute();
     }
-
-    private String pleskGetUserLoginLink(String username) throws CommandFailedException {
-        return ShellUtils.runCommand(PLESK_CLI_EXECUTABLE, "login", username).getFirst();
-    }
+    
 
     public Optional<ArrayNode> fetchSubscriptionInfo(DomainName domain) throws
             SQLException {
