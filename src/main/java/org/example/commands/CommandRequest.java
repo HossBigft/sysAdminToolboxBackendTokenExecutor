734fc9dee5fb1cd385b4001590b5e3389b2d6c68
Maxim <m.glushchenko@office.hoster.kz>
2025-04-28 10:50:08 +0500

    Add string representation

--- src/main/java/org/example/commands/CommandRequest.java
+++ src/main/java/org/example/commands/CommandRequest.java
@@ -3,8 +3,13 @@ package org.example.commands;
 
 import org.example.commands.core.AvailableCommand;
 
+import java.util.Arrays;
+
 public record CommandRequest(AvailableCommand commandName, String[] commandArgs) {
 
-
+    @Override
+    public String toString() {
+        return commandName + " args:" + Arrays.toString(commandArgs);
+    }
 }
 
